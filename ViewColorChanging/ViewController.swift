d622b7102ec726170b61e9c5d97b43ec3c6ad4df
denkitdam <cryptoden145@gmail.com>
2023-03-25 21:55:46 +0300

    Logic update: Deleted unnecessary outlets United sliders in one method

--- ViewColorChanging/ViewController.swift
+++ ViewColorChanging/ViewController.swift
@@ -13,29 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet var redColourValueLabel: UILabel!
     @IBOutlet var greenColourValueLabel: UILabel!
     @IBOutlet var blueColourValueLabel: UILabel!
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func redSliderMoved() {
-        paletteView.backgroundColor = redSlider.thumbTintColor?.withAlphaComponent(CGFloat(redSlider.value))
-        let formattedRedSliderValue = String(format: "%.2f", redSlider.value)
-        redColourValueLabel.text = formattedRedSliderValue
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        switch sender.tag {
+        case 1:
+            let formattedRedLabelValue = String(format: "%.2f", sender.value)
+            redColourValueLabel.text = formattedRedLabelValue
+        case 2:
+            let formattedGreenlabelValue = String(format: "%.2f", sender.value)
+            greenColourValueLabel.text = formattedGreenlabelValue
+        default:
+            let formattedBlueLabelValue = String(format: "%.2f", sender.value)
+            blueColourValueLabel.text = formattedBlueLabelValue
+            
+        }
     }
-    @IBAction func greenSliderMoved() {
-        paletteView.backgroundColor = greenSlider.thumbTintColor?.withAlphaComponent(CGFloat(greenSlider.value))
-        let formattedGreenSliderValue = String(format: "%.2f", greenSlider.value)
-        greenColourValueLabel.text = formattedGreenSliderValue
-    }
-    @IBAction func blueSliderMoved() {
-        paletteView.backgroundColor = blueSlider.thumbTintColor?.withAlphaComponent(CGFloat(blueSlider.value))
-        let formattedBlueSliderValue = String(format: "%.2f", blueSlider.value)
-        blueColourValueLabel.text = formattedBlueSliderValue
-    }
+  
 
 }
 
