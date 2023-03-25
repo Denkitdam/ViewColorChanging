076a242d58a5b354e843ddc72a67e674b9920f01
denkitdam <cryptoden145@gmail.com>
2023-03-25 19:55:35 +0300

    Primary logic setup

--- ViewColorChanging/ViewController.swift
+++ ViewColorChanging/ViewController.swift
@@ -9,15 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var paletteView: UIView!
     @IBOutlet var redColourValueLabel: UILabel!
     @IBOutlet var greenColourValueLabel: UILabel!
     @IBOutlet var blueColourValueLabel: UILabel!
-    @IBOutlet var paletteView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func redSliderMoved() {
+        paletteView.backgroundColor = redSlider.thumbTintColor?.withAlphaComponent(CGFloat(redSlider.value))
+        let formattedRedSliderValue = String(format: "%.2f", redSlider.value)
+        redColourValueLabel.text = formattedRedSliderValue
+    }
+    @IBAction func greenSliderMoved() {
+        paletteView.backgroundColor = greenSlider.thumbTintColor?.withAlphaComponent(CGFloat(greenSlider.value))
+        let formattedGreenSliderValue = String(format: "%.2f", greenSlider.value)
+        greenColourValueLabel.text = formattedGreenSliderValue
+    }
+    @IBAction func blueSliderMoved() {
+        paletteView.backgroundColor = blueSlider.thumbTintColor?.withAlphaComponent(CGFloat(blueSlider.value))
+        let formattedBlueSliderValue = String(format: "%.2f", blueSlider.value)
+        blueColourValueLabel.text = formattedBlueSliderValue
+    }
 
 }
 
