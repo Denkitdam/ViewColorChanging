e26afec114ddad0e928bb9990ac74b2ee7f24bff
denkitdam <cryptoden145@gmail.com>
2023-03-26 09:58:51 +0300

    logic complete

--- ViewColorChanging/ViewController.swift
+++ ViewColorChanging/ViewController.swift
@@ -7,19 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var paletteView: UIView!
+    
     @IBOutlet var redColourValueLabel: UILabel!
     @IBOutlet var greenColourValueLabel: UILabel!
     @IBOutlet var blueColourValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
   
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPaletteViewBackgroundColor()
     }
 
     @IBAction func sliderValueChanged(_ sender: UISlider) {
+        setupPaletteViewBackgroundColor()
         switch sender.tag {
         case 1:
             let formattedRedLabelValue = String(format: "%.2f", sender.value)
@@ -33,7 +40,13 @@ class ViewController: UIViewController {
             
         }
     }
-  
+    
+    private func setupPaletteViewBackgroundColor() {
+        paletteView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
 
 }
-
